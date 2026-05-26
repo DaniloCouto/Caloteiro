2ccbdfdf16c8dbb8e23fbb69b4be43b380017ea3
Danilo Soares Couto <danilo.s.couto@gmail.com>
2026-05-26 14:52:35 -0300

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-BdzYko7F.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Danilo/Documents/Projetos/Caloteiro/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-CIzIFM-F.js"] }, "/": { filePath: "C:/Users/Danilo/Documents/Projetos/Caloteiro/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-50iOmZFy.js"] } }, clientEntry: "/assets/index-CIzIFM-F.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-Du18w7yx.js";
+import "node:events";
+export {
+  w as default
+};
